388692418d21ce9627fea738f760ffafd9834dd2
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-15 10:58:48 +0300

    commit tree 7601b77df70f65ee12ea561ed1d289d80ed8cf09

--- hashketball.rb
+++ hashketball.rb
@@ -126,4 +126,112 @@ def game_hash
   }
 end
 
-# Write code here
+#HELPER METHODS
+# ===============
+
+# all players
+def all_players
+  game_hash[:home][:players] + game_hash[:away][:players] 
+end
+# puts all_players
+
+# player_stats
+def player_stats(player_name)
+  all_players.find { |player| player[:player_name] == player_name }
+end
+# puts player_stats("Kemba Walker")
+
+# helper method to find a team by their team name
+def find_team(team_name)
+  # #find on with a hash returns an array with the first key and value that match the condition
+  team_details = game_hash.find do |team_location, team_data|
+    team_data[:team_name] == team_name
+  end
+
+  # return just the value (team_data) from the .find method
+  team_details[1]
+end
+# puts find_team("Charlotte Hornets")
+
+#helper method to find the player with biggest shoe
+def big_shoe_player
+  all_players.max_by { |player| player[:shoe]}
+end
+# puts big_shoe_player
+
+# ==============================================================
+
+# 1. num_points_scored ✅
+#=====================
+def num_points_scored(player_name)
+  player = player_stats(player_name)
+  player[:points]
+end
+# puts num_points_scored("Bismack Biyombo")
+
+# 2. shoe_size ✅
+#=====================
+def shoe_size(player_name)
+  player = player_stats(player_name)
+  player[:shoe]
+end
+# puts shoe_size("Bismack Biyombo")
+
+# 3. team_colors ✅
+#=====================
+def team_colors(team_name)
+  team = find_team(team_name)
+  team[:colors]
+end
+# puts team_colors("Brooklyn Nets")
+
+# 4. team_names ✅
+#=====================
+def team_names
+  game_hash.map { |team_location, team_data| team_data[:team_name] }
+end
+# puts team_names
+
+# 5. player_numbers ✅
+#=====================
+def player_numbers(team_name)
+  team = find_team(team_name)
+  team[:players].map {|player| player[:number]}
+end
+# puts player_numbers("Brooklyn Nets")
+
+# 7. big_shoe_rebounds ✅
+#=====================
+def big_shoe_rebounds
+  big_shoe_player[:rebounds]
+end
+puts big_shoe_rebounds
+
+# 8. most_points_scored
+#=====================
+def most_points_scored(player_name)
+  
+end
+# puts most_points_scored("Bismack Biyombo")
+
+# 9. winning_team
+#=====================
+def winning_team(team_name)
+ 
+end
+# puts winning_team("Brooklyn Nets")
+
+# 10. player_with_longest_name
+#=====================
+def player_with_longest_name(player_name)
+ 
+end
+# puts player_with_longest_name("Bismack Biyombo")
+
+# 11. long_name_steals_a_ton
+#=====================
+def long_name_steals_a_ton(player_name)
+  
+end
+# puts long_name_steals_a_ton("Bismack Biyombo")
+
